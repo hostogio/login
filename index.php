007d3c80acca8f659f3464d7dfe5cd2bf04b34c0
Hostogio <hostogio_oliveira@estudante.sc.senai.br>
2019-09-30 21:17:58 -0300

    Formulário de cadastro de novo usuário
    close #3

--- index.php
+++ index.php
@@ -16,6 +16,12 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Sistema de Login</title>
+    <style>
+      #alerta, #caixaSenha, #caixaRegistro{
+        display: none;
+        
+        }   
+    </style>
 </head>
 
 <body class="bg-dark">
@@ -77,8 +83,7 @@
 
         <!-- Formulário de Recuperação de senha -->
         <section class="row mt-5">
-            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
-
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaSenha">
                 <h2>
                     Gerar Nova Senha
                 </h2>
@@ -131,7 +136,34 @@
                     <div class="form-group">
                         <input type="password" name="senhaUsuario" id="senhaUsuario" class="form-control" placeholder="Senha">
                     </div>
+
+                    <div class="form-group">
+                        <input type="password" name="senhaUsuarioConfirmar" id="senhaUsuarioConfirmar" class="form-control" placeholder="Confirmar senha">
+                    </div>
+
+                    <div class="form-group">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" name="concordar" id="concordar" class="custom-control-input">
+                            <label for="concordar" class="custom-control-label">
+                                Eu concordo com <a href="#">os termos e condições. </a>
+                            </label>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="submit" value="::Registrar::" name="btnRegistrar" id="btnregistrar" class="btn btn-primary btn-block">
+                        </div>
+
+                    <div class="form-group">
+                        <p class="text-center">
+                            Novo usuário?
+                            <a href="#" id="btnRegistrarNovo">
+                            Registre-se aqui!
+                            </a>
+                        </p>
+                    </div>
+
                 </form>
+
 
             </div>
 
@@ -149,6 +181,10 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <script>
+     //Código jQuery para mostrar e ocultar os formulários
+    </script>
 </body>
 
 </html>
