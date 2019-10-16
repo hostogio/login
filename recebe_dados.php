007a165e1a3a6d7e28a486073fabe888ea994f4a
hostogio <hostogio_oliveira@estudante.sc.senai.br>
2019-10-15 21:03:34 -0300

    Update recebe_dados.php

--- recebe_dados.php
+++ recebe_dados.php
@@ -9,14 +9,35 @@ function verificar_entrada($entrada){
     $saida = trim($saida);
     return $saida;//retorna saída limpa
 }
+
 //Teste se existe a ação
 if(isset($_POST['action'])){
-    if($_POST['action'] == 'cadastro'){
+    if($_POST['action'] = 'cadastro') {
         //Teste se ação é igual a cadastro
-        echo "\n<p>cadastro</p>";
-        echo "\n<pre>";//Pre-formatar
-        print_r($_POST);
-        echo "\n<\pre>";
+        # echo "\n<p>cadastro</p>";
+        # echo "\n<pre>";//Pre-formatar
+        # print_r($_POST);
+        # echo "\n<\pre>";
+        //pegando dados do formulário
+        $nomeCompleto = verificar_entrada ($_POST['nomeCompleto']);
+        $nomeDoUsuario = verificar_entrada ($_POST['nomeDoUsuario']);
+        $emailUsuario = verificar_entrada ($_POST['emailUsuario']);
+        $senhaDoUsuario = verificar_entrada($_POST['senhaDoUsuario']);
+        $senhaUsuarioConfirmar = verificar_entrada($_POST['senhaUsuarioConfirmar']);
+
+        $dataCriado = date("Y-m-d"); //Data atual no formato Banco de dados
+
+        //codificando as senhas
+        $senhaCodificada = sha1($senhaDoUsuario);
+        $senhaConfirmarCod = sha1($senhaUsuarioConfirmar);
+        //Teste de captura de dados
+        echo "<p>Nome completo: $nomeCompleto</p>";
+        echo "<p>Nome de Usuário: $nomeDoUsuario</p>";
+        echo "<p>E-mail: $emailUsuario</p>";
+        echo "<p>Senha codificada: $senhaCodificada</p>";
+        echo "<p>Data de Criação: $dataCriado</p>";
+
+
     }else if($_POST['action'] == 'login'){
         //Senão, teste se ação é login
         echo "\n<p>login</p>";
